1a398b5f58b02cf479bca0e900366449c3abd37d
hojunking96 <hojunking96@gmail.com>
2023-04-24 13:40:54 +0900

    Hello World 출력 및 GetMapping 구현

--- src/main/java/com/example/practice_0424/MainController.java
+++ src/main/java/com/example/practice_0424/MainController.java
@@ -1,10 +1,13 @@
 package com.example.practice_0424;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+
     @GetMapping("/")
     public String hello() {
         return "main";
