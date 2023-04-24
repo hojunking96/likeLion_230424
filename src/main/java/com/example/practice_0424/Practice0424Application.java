424387a7db1e9824dee3a7a5014a8a773aecba8d
hojunking96 <hojunking96@gmail.com>
2023-04-24 13:23:06 +0900

    세팅

---
+++ src/main/java/com/example/practice_0424/Practice0424Application.java
@@ -0,0 +1,13 @@
+package com.example.practice_0424;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice0424Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practice0424Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practice_0424/Practice0424ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_0424;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice0424ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
